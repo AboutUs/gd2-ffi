b76c758965bec943fd6ab31f50204305fed1308b
Brandon CS Sanders <brandon@thesanders.us>
2013-11-04 21:01:49 -0800

    make it work with libgd.3.so (still gd2 counterintuitively)

--- lib/gd2/cgd2.rb
+++ lib/gd2/cgd2.rb
@@ -2,7 +2,7 @@ module CGD2
   
   extend FFI::Library
   ffi_lib_flags :now, :global
-  ffi_lib [FFI.map_library_name("libgd.2"), FFI.map_library_name("libgd.so.2")]
+  ffi_lib [FFI.map_library_name("libgd.2"), FFI.map_library_name("libgd.so.2"), FFI.map_library_name("libgd.3"), FFI.map_library_name("libgd.so.3")]
 
   # BGD_DECLARE(gdImagePtr) gdImageCreate (int sx, int sy)
   attach_function :gdImageCreate, [:int, :int], :pointer
